a1a37000a9ae2d840968a2a1939ff02316525a66
Blair Anderson <blair.l.anderson@gmail.com>
2015-05-19 21:35:06 -0300

    Initial Creation of first sub-tool --> Name Generator + dimensions (796x575 for 800x600 with menustrip)

---
+++ DD-NPCs/NameGeneratorUI/NameGen_Splash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameGeneratorUI
+{
+    public partial class NameGen_Splash : Form
+    {
+        public NameGen_Splash()
+        {
+            InitializeComponent();
+        }
+    }
+}
